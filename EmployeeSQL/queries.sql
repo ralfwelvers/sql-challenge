ad9f6ee1900c6bd8c33d3c29e103e7fac8da8a18
ralfwelvers <ralf.welvers@gmail.com>
2023-07-01 14:05:33 -0500

    first commit

---
+++ EmployeeSQL/queries.sql
@@ -0,0 +1,51 @@
+﻿-- Ralf Welvers
+-- Homework 9
+-- SQL queries for data analysis
+
+-- 1) 
+select e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+from employee e
+inner join salary s on e.emp_no = s.emp_no;
+
+-- 2)
+select e.first_name, e.last_name, e.hire_date
+from employee e
+where date_part('YEAR',e.hire_date) = 1986;
+
+-- 3)
+select e.emp_no, e.first_name, e.last_name, dm.dept_no, dt.dept_name
+from dept_manager dm
+inner join employee e on dm.emp_no = e.emp_no
+inner join department dt on dm.dept_no = dt.dept_no;
+
+-- 4)
+select e.emp_no, e.first_name, e.last_name, de.dept_no, dt.dept_name
+from employee e
+inner join dept_emp de on e.emp_no = de.emp_no
+inner join department dt on de.dept_no = dt.dept_no;
+
+-- 5)
+select e.first_name, e.last_name, e.hire_date
+from employee e
+where e.first_name = 'Hercules' and e.last_name like 'B%';
+
+-- 6)
+select e.emp_no, e.first_name, e.last_name
+from employee e
+inner join dept_emp de on e.emp_no = de.emp_no
+inner join department dt on de.dept_no = dt.dept_no
+where dt.dept_no = 'd007';
+
+-- 7)
+select e.emp_no, e.first_name, e.last_name, dt.dept_name
+from employee e
+inner join dept_emp de on e.emp_no = de.emp_no
+inner join department dt on de.dept_no = dt.dept_no
+where dt.dept_no in ('d007','d005');
+
+-- 8)
+select e.last_name, count(e.last_name) as last_name_count
+from employee e
+group by e.last_name
+order by last_name_count desc;
+
---
+++ EmployeeSQL/table schemata.sql
@@ -0,0 +1,75 @@
+﻿-- Ralf Welvers
+-- Homework 9
+-- table schemata
+
+CREATE TABLE "employee" (
+    "emp_no" varchar(6)   NOT NULL,
+    "emp_title_id" varchar(5)   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "first_name" varchar(200)   NOT NULL,
+    "last_name" varchar(200)   NOT NULL,
+    "sex" varchar(1)   NOT NULL,
+    "hire_date" date   NOT NULL,
+    CONSTRAINT "pk_employee" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "title" (
+    "title_id" varchar(5)   NOT NULL,
+    "title" varchar(200)   NOT NULL,
+    CONSTRAINT "pk_title" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+CREATE TABLE "department" (
+    "dept_no" varchar(4)   NOT NULL,
+    "dept_name" varchar(200)   NOT NULL,
+    CONSTRAINT "pk_department" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" varchar(6)   NOT NULL,
+    "dept_no" varchar(4)   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" varchar(4)   NOT NULL,
+    "emp_no" varchar(6)   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "dept_no","emp_no"
+     )
+);
+
+CREATE TABLE "salary" (
+    "emp_no" varchar(6)   NOT NULL,
+    "salary" money   NOT NULL,
+    CONSTRAINT "pk_salary" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+ALTER TABLE "employee" ADD CONSTRAINT "fk_employee_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "title" ("title_id");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employee" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "department" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "department" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employee" ("emp_no");
+
+ALTER TABLE "salary" ADD CONSTRAINT "fk_salary_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employee" ("emp_no");
+
